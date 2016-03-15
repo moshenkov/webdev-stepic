e9d5732b680a559ed94723c6521bddbf82988165
moshenkov <moshenkov.dmitry@gmail.com>
2016-03-15 16:25:40 +0300

    django

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-sudo gunicorn -b 0.0.0.0:8080 hello  &
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application
